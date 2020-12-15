cd934f5a074af58bd5c1d8a8339476343a9ad5d9
starpro123 <starpro123@naver.com>
2020-12-15 14:11:38 +0000

    getmatchData.py

--- ConstURL.py
+++ ConstURL.py
@@ -1,3 +1,3 @@
 class ConstURL:
     mainUrl = "https://kr.api.riotgames.com"
-    api = "RGAPI-c3acb177-4ff3-4aaf-a134-3bba00424a22"
+    apiKey = "RGAPI-c3acb177-4ff3-4aaf-a134-3bba00424a22"
--- getmatchData.py
+++ getmatchData.py
@@ -1,8 +1,28 @@
 import pandas as pd
-
+import time
+from requests import get
 from ConstURL import ConstURL
 
 ConstURL = ConstURL()
-print(ConstURL.api)
+apiKey = ConstURL.apiKey
+mainUrl = ConstURL.mainUrl
+
+def getMatchlists(accountId, endIndex = "30"):
+    return get(mainUrl + "/lol/match/v4/matchlists/by-account/"+accountId+"?endIndex="+endIndex+"&api_key=" +apiKey).json()
+
 prodata = pd.read_csv('nickplusID.csv',index_col= 0 ,header=0)
+
+
+
+for nickname, accountId in zip(prodata["nickname"], prodata["accountId"]):
+    try:
+        matches = getMatchlists(accountId,"30")['matches']
+        matchDf = pd.DataFrame(matches)
+        matchDf.to_csv(f"player/{nickname}.csv", mode='w')
+    except:
+        matchDf = pd.DataFrame()
+        matchDf.to_csv(f"fail/Fail_{nickname}.csv", mode='w')
+        pass
+    time.sleep(1.3)
+
 
