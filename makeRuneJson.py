0f9526b33cba700db1abb14602f3ba4e2bd382b2
starpro123 <starpro123@naver.com>
2020-12-18 14:14:38 +0000

    make rune.json

---
+++ makeRuneJson.py
@@ -0,0 +1,30 @@
+from requests import get
+import json
+runeLink = "http://ddragon.leagueoflegends.com/cdn/10.24.1/data/ko_KR/runesReforged.json"
+# print(get(runeLink).json())
+runeJson = get(runeLink).json()
+
+data = runeJson
+runeDict = {}
+for i in data:
+    # runekey = data[i]["id"]       233 champion이랑은 다름
+    # runeEname = data[i]["key"]                 Aatrox
+    # runeKname = data[i]["name"] 아트록스
+
+    runekey = i["id"]
+    runeEname = i["key"]
+    runeKname = i["name"]
+    print(runeKname)    
+    runeDict[runekey] = {"runeEname":runeEname, "runeKname":runeKname}
+    for j in i["slots"]:
+        for k in j["runes"]:
+            runekey = k["id"]
+            runeEname = k["key"]
+            runeKname = k["name"]       
+            print(runeKname)
+            runeDict[runekey] = {"runeEname":runeEname, "runeKname":runeKname}
+with open("rune.json", "w") as outfile:  
+    json.dump(runeDict, outfile) 
+
+
+
